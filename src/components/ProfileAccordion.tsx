46120d353edaefa216bceb7848c6f3e0fe17e8b9
Sean Johnson <sean.johnson@zenhub.com>
2024-07-11 12:37:31 -0400

    Complete top section but still need reponsive design

--- src/components/ProfileAccordion.tsx
+++ src/components/ProfileAccordion.tsx
@@ -23,8 +23,8 @@ const AccordionComponent = () => {
   return (
     <div style={{ maxWidth: 600 }}>
       <Typography
-        mb={3}
         variant="h5"
+        mb={2}
         align="center"
         gutterBottom
         sx={{ color: 'var(--text-primary)' }}
@@ -42,19 +42,12 @@ const AccordionComponent = () => {
           }
           aria-controls="panel1a-content"
           id="panel1a-header"
-          sx={{
-            backgroundColor: 'var(--background-paper)',
-            ...getExpandButtonDetails('panel1'),
-          }}
+          sx={getExpandButtonDetails('panel1')}
         >
           <Typography sx={getExpandButtonDetails('panel1')}>Who Am I</Typography>
         </AccordionSummary>
-        <AccordionDetails
-          sx={{
-            backgroundColor: 'var(--background-paper)',
-          }}
-        >
-          <Typography>
+        <AccordionDetails>
+          <Typography mt={-3} sx={{ fontSize: 14 }}>
             Software engineer based in Philadelphia, PA. Proven track record of leading technical
             projects and efficiently delivering quality results. Pivoted to software development in
             2020, inspired by a passion for technology after collaborating with engineering teams in
@@ -74,24 +67,18 @@ const AccordionComponent = () => {
           }
           aria-controls="panel2a-content"
           id="panel2a-header"
-          sx={{
-            backgroundColor: 'var(--background-paper)',
-            ...getExpandButtonDetails('panel2'),
-          }}
+          sx={getExpandButtonDetails('panel2')}
         >
           <Typography sx={getExpandButtonDetails('panel2')}>Current Role</Typography>
         </AccordionSummary>
-        <AccordionDetails
-          sx={{
-            backgroundColor: 'var(--background-paper)',
-          }}
-        >
-          <Typography variant="subtitle2">Intermediate Frontend Engineer @ Zenhub</Typography>
-          Collaborated with product managers, designers, and engineers to build robust project
-          management suite. Led projects and delivered impactful results through effective
-          communication, collaboration, and my technical expertise. Developed core product features
-          that drive user growth and retention, including AI-powered enhancements that streamline
-          key user processes.
+        <AccordionDetails>
+          <Typography mt={-3} sx={{ fontSize: 14 }}>
+            <strong>Intermediate Frontend Engineer @ Zenhub: </strong>Collaborated with product
+            managers, designers, and engineers to build robust project management suite. Led
+            projects and delivered impactful results through effective communication, collaboration,
+            and my technical expertise. Developed core product features that drive user growth and
+            retention, including AI-powered enhancements that streamline key user processes.
+          </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
@@ -105,19 +92,12 @@ const AccordionComponent = () => {
           }
           aria-controls="panel4a-content"
           id="panel4a-header"
-          sx={{
-            backgroundColor: 'var(--background-paper)',
-            ...getExpandButtonDetails('panel4'),
-          }}
+          sx={getExpandButtonDetails('panel4')}
         >
           <Typography sx={getExpandButtonDetails('panel4')}>Teamwork</Typography>
         </AccordionSummary>
-        <AccordionDetails
-          sx={{
-            backgroundColor: 'var(--background-paper)',
-          }}
-        >
-          <Typography>
+        <AccordionDetails>
+          <Typography mt={-3} sx={{ fontSize: 14 }}>
             Team-oriented leader who builds outstanding relationships with clients and internal
             stakeholders by leveraging highly effective communication and interpersonal skills. I
             enjoy collaborating with and learning from other developers, and am a proponent of a
@@ -137,25 +117,16 @@ const AccordionComponent = () => {
           }
           aria-controls="panel5a-content"
           id="panel5a-header"
-          sx={{
-            backgroundColor: 'var(--background-paper)',
-            ...getExpandButtonDetails('panel5'),
-          }}
+          sx={getExpandButtonDetails('panel5')}
         >
           <Typography sx={getExpandButtonDetails('panel5')}>Personal Facts</Typography>
         </AccordionSummary>
-        <AccordionDetails
-          sx={{
-            backgroundColor: 'var(--background-paper)',
-          }}
-        >
-          <Typography component="div">
+        <AccordionDetails>
+          <Typography mt={-4} sx={{ fontSize: 14 }}>
             <ul>
               <li>Frontend Engineer @ ZenHub since October 2021</li>
-              <li>Thinkful Software Engineering Immersive Program 2021 graduate</li>
               <li>Career pivot to software development in 2020</li>
               <li>NYU Stern School of Business 2018 MBA graduate</li>
-              <li>10 years in financial services roles</li>
               <li>Enjoy exercising, traveling, and reading</li>
             </ul>
           </Typography>
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,116 +1,130 @@
-import React, { useState } from 'react';
-import {
-  Box,
-  Card,
-  CardContent,
-  Typography,
-  Stepper,
-  Step,
-  StepButton,
-  Popover,
-} from '@mui/material';
+import { useState, useEffect, useRef } from 'react';
+import { Box, Card, CardContent, Typography, Stepper, Step, StepButton } from '@mui/material';
 
 const timelineEvents = [
   {
     year: '2011',
     title: 'Undergraduate Degree',
     description:
-      'Graduated from the University of Connecticut with a 3.6 GPA and a Bachelor of Science degree. Started as a Financial Analyst at Citco Fund Services on the Derivative Reconciliations team.',
+      'Graduated from the University of Connecticut with a 3.6 GPA and a Bachelor of Science degree. Started as a Financial Analyst at Citco Fund Services.',
   },
   {
     year: '2013',
-    title: 'Promotion',
-    description: 'Promoted to Senior Financial Analyst at Citco Fund Services.',
+    title: 'Financial Services Promotion',
+    description:
+      'Promoted to Senior Financial Analyst at Citco Fund Services. Collaborated with engineers to build reconciliation platform.',
   },
   {
     year: '2015',
     title: 'Began Graduate School',
     description:
-      'Started MBA program at NYU Stern School of Business. Promoted to Fund Associate at Citco Fund Services',
+      'Started MBA program at NYU Stern School of Business. Promoted to Fund Associate at Citco Fund Services.',
   },
   {
     year: '2018',
-    title: 'Graduate School Degree',
+    title: 'Graduate Degree',
     description:
-      'Graduated from NYU Stern School of Business with a 3.5 GPA and a Master of Business Administration Degree. Pivoted to Loan Services Team at Citco.',
+      'Graduated from NYU Stern School of Business with a 3.5 GPA and a Master of Business Administration Degree.',
   },
   {
     year: '2020',
     title: 'Career Pivot',
-    description:
-      'Decided to pursue a career in software engineering after developing a passion for programming. Enrolled in the Thinkful Software Engineering Immersive Program.',
+    description: `Decided to pursue a career in software engineering after developing a passion for programming. Enrolled in Thinkful's Software Engineering Immersive Program.`,
   },
   {
     year: '2021',
     title: 'Software Engineer',
     description:
-      'Graduated from the Thinkful Software Engineering Immersive Program. Started as an Associate Frontend Engineer at Zenhub.',
+      'Graduated from engineering bootcamp. Started as an Associate Frontend Engineer at Zenhub. Modernized tech stack and improved web app performance.',
   },
   {
     year: '2023',
-    title: 'Promotion',
+    title: 'Software Engineering Promotion',
     description:
-      'Promoted to Intermediate Frontend Engineer at Zenhub and began leading technical projects.',
+      'Promoted to Intermediate Frontend Engineer at Zenhub. Led technical projects and delivered key AI features to automate core user processes.',
   },
 ];
 
 const Timeline = () => {
-  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
-  const [selectedEvent, setSelectedEvent] = useState<any | null>(null);
+  const [selectedEvent, setSelectedEvent] = useState<any>(timelineEvents[0]);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
-  const handlePopoverOpen = (event: React.MouseEvent<HTMLElement>, timelineEvent: any) => {
-    setAnchorEl(event.currentTarget);
+  const handleStepClick = (timelineEvent: any) => {
     setSelectedEvent(timelineEvent);
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      startInterval();
+    }
   };
 
-  const handlePopoverClose = () => {
-    setAnchorEl(null);
-    setSelectedEvent(null);
+  const startInterval = () => {
+    intervalRef.current = setInterval(() => {
+      setSelectedEvent((prevEvent) => {
+        const currentIndex = timelineEvents.findIndex((event) => event.year === prevEvent.year);
+        const nextIndex = (currentIndex + 1) % timelineEvents.length;
+        return timelineEvents[nextIndex];
+      });
+    }, 5000);
   };
 
-  const open = Boolean(anchorEl);
+  useEffect(() => {
+    startInterval();
+
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, []);
 
   return (
-    <Box sx={{ width: '100%' }}>
+    <Box sx={{ maxWidth: 600, width: '100%' }}>
       <Typography
-        mb={3}
         variant="h5"
         align="center"
+        mb={0}
         gutterBottom
         sx={{ color: 'var(--text-primary)' }}
       >
         Professional Timeline
       </Typography>
-      <Stepper alternativeLabel nonLinear>
+      <Stepper alternativeLabel nonLinear sx={{ marginBottom: -1 }}>
         {timelineEvents.map((event, index) => (
-          <Step key={index}>
-            <StepButton onMouseEnter={(e) => handlePopoverOpen(e, event)}>{event.year}</StepButton>
-          </Step>
+          <StepButton
+            onClick={() => handleStepClick(event)}
+            sx={{
+              '& .MuiStepLabel-root .MuiStepIcon-root': {
+                color: selectedEvent?.year === event.year ? 'var(--primary-color)' : 'gray',
+                fontWeight: selectedEvent?.year === event.year ? 'bold' : 'normal',
+                transform: selectedEvent?.year === event.year ? 'scale(1.2)' : 'none',
+                transition: 'transform 0.1s ease, color 0.1s ease, font-weight 0.1s ease',
+              },
+              '& .MuiStepLabel-label': {
+                fontWeight: selectedEvent?.year === event.year ? '600' : '500',
+                color:
+                  selectedEvent?.year === event.year
+                    ? 'var(--inverse-text-primary)'
+                    : 'var(--inverse-text-secondary)',
+                backgroundColor: 'var(--primary-color)',
+                borderRadius: 5,
+                padding: 1,
+              },
+            }}
+          >
+            {event.year}
+          </StepButton>
         ))}
       </Stepper>
-      <Popover
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handlePopoverClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-        transformOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-        PaperProps={{
-          sx: { maxWidth: 300 },
-        }}
-      >
-        <Card>
-          <CardContent>
-            <Typography variant="h6">{selectedEvent?.title}</Typography>
-            <Typography variant="body2">{selectedEvent?.description}</Typography>
-          </CardContent>
-        </Card>
-      </Popover>
+      {selectedEvent && (
+        <Box mt={3}>
+          <Card>
+            <CardContent>
+              <Typography variant="h6">{selectedEvent.title}</Typography>
+              <Typography variant="body2">{selectedEvent.description}</Typography>
+            </CardContent>
+          </Card>
+        </Box>
+      )}
     </Box>
   );
 };
